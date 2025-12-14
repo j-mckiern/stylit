619c03fd20dd31c880377fd05a52ffbe3e0499da
Jacob <138262144+j-mckiern@users.noreply.github.com>
2025-12-14 18:32:13 -0500

    refactor: seperate GET profile routes

--- backend/routes/profile_routes.py
+++ backend/routes/profile_routes.py
@@ -15,7 +15,7 @@ service = ProfileService()
 
 @router.post("/", summary="Create user profile", status_code=201)
 async def create_profile(
-    id: str = Form(...),
+    current_user: User = Depends(get_current_user),
     username: str = Form(...),
     display_name: str = Form(...),
     visibility: str = Form(...),
@@ -25,22 +25,42 @@ async def create_profile(
     Create profile in the 'profiles' table.
     """
     profile = ProfileCreateRequest(
-        id=id,
+        id=current_user.id,
         username=username,
         display_name=display_name,
         visibility=visibility,
     )
-    response = await service.create_profile(profile, pfp)
-    return response
+    return await service.create_profile(profile, pfp)
 
+@router.get("/verify")
+async def verify_user(current_user: User = Depends(get_current_user)):
+    return {"message": f"Hello, {current_user.id}! You have access to protected data."}
+
+@router.get("/me", summary="Get user profile by id", response_model=list[ProfileResponse])
+async def get_profiles(
+    current_user: User = Depends(get_current_user)
+):
+    """
+    Fetch profiles by id from the 'profiles' table.
+    """
+    return await service.read_profiles(profile_id=current_user.id)
 
 @router.get("/", summary="Get user profiles", response_model=list[ProfileResponse])
-async def get_profiles(username: str | None = None, profile_id: str | None = None):
+async def get_profiles():
     """
     Fetch profiles from the 'profiles' table.
     """
-    response = await service.read_profiles(username, profile_id)
-    return response
+    return await service.read_profiles()
+
+
+@router.get("/{username}", summary="Get user profiles by username", response_model=list[ProfileResponse])
+async def get_profiles(
+    username: str
+):
+    """
+    Fetch profiles by username from the 'profiles' table.
+    """
+    return await service.read_profiles(username=username)
 
 
 @router.put("/", summary="Update user profile", status_code=204)
@@ -59,8 +79,7 @@ async def update_profile(
         display_name=display_name,
         visibility=visibility,
     )
-    response = await service.update_profile(profile_id, profile, pfp)
-    return response
+    return await service.update_profile(profile_id, profile, pfp)
 
 
 @router.delete("/", summary="Delete user profile", status_code=204)
@@ -68,9 +87,4 @@ async def delete_profile(profile_id: str):
     """
     Delete profile from the 'profiles' table.
     """
-    response = await service.delete_profile(profile_id)
-    return response
-
-@router.get("/verify")
-async def verify_user(current_user: User = Depends(get_current_user)):
-    return {"message": f"Hello, {current_user.email}! You have access to protected data."}
+    return await service.delete_profile(profile_id)
